1ff9ea42492cff4fedf7f41de0bac4f9e6702e80
Disaylt <disofban@gmail.com>
2025-03-25 23:12:40 +0300

    change aggregator

--- src/components/layout/sidebars/AggregateSideBar.vue
+++ src/components/layout/sidebars/AggregateSideBar.vue
@@ -1,14 +1,5 @@
 <template>
     <div>{{ name }}</div>
-    <div>{{ name }}</div>
-    <div>{{ name }}</div>
-    <div>{{ name }}</div>
-    <div>{{ name }}</div>
-    <div>{{ name }}</div>
-    <div>{{ name }}</div>
-    <div>{{ name }}</div>
-    <Button @click="userService.logout()" type="button" label="logout" icon="pi pi-user" />
-    <Button @click="getDetails()" type="button" label="getdetails" icon="pi pi-user" />
 </template>
 
 <script setup lang="ts">
@@ -17,16 +8,6 @@ import userService from '../../../services/auth/user.service';
 
 const name = ref<string>("Агрегатор");
 
-const getDetails = async () => {
-
-    await userService.getInfo()
-        .then((resp) => {
-            console.log(resp)
-        })
-        .catch(() => {
-            
-        })
-}
 
 </script>
 
